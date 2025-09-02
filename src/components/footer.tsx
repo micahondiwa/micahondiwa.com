077df8d0e1e239dc2eea1f71e50409583d2a86e3
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-09-02 10:09:15 +0300

    Blog post link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,84 +1,87 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHackerrank } from "@fortawesome/free-brands-svg-icons"
-import { faGraduationCap } from "@fortawesome/free-solid-svg-icons"
+import { faGraduationCap, faPenNib } from "@fortawesome/free-solid-svg-icons"
 import {
-    faTwitter,
     faGithub,
     faLinkedin,
     faStackOverflow,
+    faXTwitter,
 } from "@fortawesome/free-brands-svg-icons"
-import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons" // external link icon
 
 const Footer = () => (
     <footer className="footer">
         <div className="container">
-            <ul className="footer__social-list">
+            <ul className="footer__social-list flex gap-4 justify-center">
 
-                {/* CV link */}
                 <li>
                     <a
-                        href="https://example.com/micahondiwa-cv.pdf"
+                        href="https://drive.google.com/file/d/1J_qfyGeOhmJPCycbt3-ANrUBeZn9aJer/view?usp=sharing"
                         target="_blank"
                         rel="noreferrer"
-                        className="footer__social-list-icon"
-                        aria-label="Download CV"
-                        style={{ display: "inline-flex", alignItems: "center", gap: "4px", textDecoration: "none" }}
-                    >CV
+                        title="Download CV"
+                        className="footer__social-list-icon no-underline hover:no-underline"
+                        style={{ textDecoration: "none" }}
+                    >
+                        CV
                     </a>
-
                 </li>
+
                 <li>
                     <a
                         href="https://github.com/micahondiwa"
                         target="_blank"
                         rel="noreferrer"
+                        title="GitHub"
                         className="footer__social-list-icon"
-                        aria-label="GitHub profile"
                     >
                         <FontAwesomeIcon icon={faGithub} />
                     </a>
                 </li>
+
                 <li>
                     <a
                         href="https://www.linkedin.com/in/micahondiwa/"
                         target="_blank"
                         rel="noreferrer"
+                        title="LinkedIn"
                         className="footer__social-list-icon"
-                        aria-label="LinkedIn profile"
                     >
                         <FontAwesomeIcon icon={faLinkedin} />
                     </a>
                 </li>
+
                 <li>
                     <a
                         href="https://x.com/micahondiwa"
                         target="_blank"
                         rel="noreferrer"
+                        title="X"
                         className="footer__social-list-icon"
-                        aria-label="Twitter profile"
                     >
-                        <FontAwesomeIcon icon={faTwitter} />
+                        <FontAwesomeIcon icon={faXTwitter} />
                     </a>
                 </li>
+
                 <li>
                     <a
                         href="https://stackoverflow.com/users/15467774/micahondiwa"
                         target="_blank"
                         rel="noreferrer"
+                        title="Stack Overflow"
                         className="footer__social-list-icon"
-                        aria-label="Stack Overflow profile"
                     >
                         <FontAwesomeIcon icon={faStackOverflow} />
                     </a>
                 </li>
+
                 <li>
                     <a
                         href="https://www.hackerrank.com/profile/micahondiwa"
                         target="_blank"
                         rel="noreferrer"
+                        title="HackerRank"
                         className="footer__social-list-icon"
-                        aria-label="HackerRank profile"
                     >
                         <FontAwesomeIcon icon={faHackerrank} />
                     </a>
@@ -89,15 +92,27 @@ const Footer = () => (
                         href="https://surl.li/orlzun"
                         target="_blank"
                         rel="noreferrer"
+                        title="Google Scholar"
                         className="footer__social-list-icon"
-                        aria-label="Google Scholar profile"
                     >
                         <FontAwesomeIcon icon={faGraduationCap} />
                     </a>
                 </li>
 
+                <li>
+                    <a
+                        href="https://micahondiwa.hashnode.dev/"
+                        target="_blank"
+                        rel="noreferrer"
+                        title="Blog (Hashnode)"
+                        className="footer__social-list-icon"
+                    >
+                        <FontAwesomeIcon icon={faPenNib} />
+                    </a>
+                </li>
+
             </ul>
-            <p className="footer__copyright">
+            <p className="footer__copyright text-center mt-4">
                 &copy; Micah Ondiwa {new Date().getFullYear()}
             </p>
         </div>
